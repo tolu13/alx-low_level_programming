13200813909a028aee80da703f8fa5b36457e2f9
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-15 14:38:36 -0700

    size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,5 @@ int main(void)
 			printf("Size of long int: %d byte(s)\n", sizeof(long int));
 				printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 					printf("Size of a float: %d byte(s)\n", sizeof(float));
-					return (0);
+						return (0);
 }
