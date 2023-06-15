0d9e6593ee3ed5f28ec44318cf7cd01f01f43f6b
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-15 08:46:02 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a c program using printf
+ *
+ * Return :0
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the kutcome is a piece of art");
+	return (0);
+}
