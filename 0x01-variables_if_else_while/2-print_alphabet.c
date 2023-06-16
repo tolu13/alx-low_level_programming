a7144b7fed04e9cf1fe12fd42430b91135d056a7
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-16 11:51:41 -0700

    if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase
+ *
+ * Return: Always o
+ *
+ */
+int main(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+		putchar(t);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
