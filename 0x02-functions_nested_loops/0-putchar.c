ee838cc870d93c5b75f4020a75149012005b36f9
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-20 06:12:21 -0700

    using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints putchar followed by a newline
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n == _putchar)
+	{
+		putchar(n);
+	}
+}
