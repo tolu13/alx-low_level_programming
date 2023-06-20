50bbfdfe4b48b70730da82cf07e9bd6152d86af1
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-20 09:11:56 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - prints putchar followed by a newline
@@ -20,4 +21,5 @@ int main(void)
 	putchar('\n');
 
 	return (0);
+
 }
