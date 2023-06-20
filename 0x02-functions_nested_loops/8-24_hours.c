97c4d89575a0f64b2a2af3b34871622d062fc73d
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-20 15:40:09 -0700

    prints mins

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes from 00:00 to 23:59
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int k, u;
+
+	k = 0;
+	while (k < 24)
+	{
+		u = 0;
+		while (u < 0)
+		{
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+			_putchar (':');
+			_putchar((u / 10) + '0');
+			_putchar((u % 10) + '0');
+			u++;
+		}
+	}
+	k++;
+}
