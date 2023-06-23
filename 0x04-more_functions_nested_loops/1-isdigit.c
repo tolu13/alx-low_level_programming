f46da37f2dbfa806031f4b7aae928b231531a0c2
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-22 18:47:04 -0700

    cool

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * int_isdigit - checks if its a number or letter
+ * _isdigit - checks if its a number or letter
  *
- * Return: 1 if its a numberband 0 fot otherwise
-  */
+ * Return: 1 if its a numberband 0 fot otherwis
+ */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 58)
 	{
 		return (1);
 	}
-	else
 	{
 		return (0);
 	}
