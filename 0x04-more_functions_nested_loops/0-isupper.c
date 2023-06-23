debf06cba10b865867cb3b813a01c7824abf4b2f
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-22 18:37:53 -0700

    phheew

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 /**
  * _isupper -checks if a letter is uppercase
  *@c: shouldvreturn uppercase
- *Return: 1
+ *Return: 1 for uppercase and 0 for otherwise
  */
 int _isupper(int c)
 {
 
 
-	if (c == 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
