f0081d77a5daf1d57aa66171f9f8d50e8d42bf41
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-22 18:50:51 -0700

    cool

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 
 /**
  * _isdigit - checks if its a number or letter
- *
+ *@c: character to be checked 
  * Return: 1 if its a numberband 0 fot otherwis
  */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 58)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
