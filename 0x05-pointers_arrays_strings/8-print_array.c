31921e1303a4a304111f4dda19d027755343c83e
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-26 13:41:19 -0700

    cool

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - function to print arrays
+ * @a: assigned int
+ * @n: second assigned int
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (1 = 0; i < n; i++)
+	{
+	if (i != n - 1)
+	{
+	printf("%d"; a[i]);
+	}
+	else 
+	printf("%d", a[i]);
+	}
+	printf("\n");
+}
+
