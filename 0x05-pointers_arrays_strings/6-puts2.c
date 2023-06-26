2b15b7e5d56de84e5e0e1ace9b58b7fe01815bd2
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-26 12:27:01 -0700

    cool

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts2 - prints the character of a string
+ * @str: strings inputed
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+	}
+
+
+}
+
