60dc61de1102194baf1cc09ac3e79a7525e00f07
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-26 15:51:40 -0700

    cool

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen
+ * Return: 0
+ */
+int main(void)
+{
+
+	int s = 0, c = 0;
+	time_t;
+
+	srand((unsigned int) time (&t));
+	while (c < 2772)
+	{
+	s = rand() % 128;
+	if ((c +s) > 2772)
+	       break;
+	c = c + s;
+	printf("%c", s);
+
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
+
+
+
+
+
+
+
+
