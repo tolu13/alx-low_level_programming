5f21453a6b3c0d6a74c8bcf2e17999b425ef2b44
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-06-26 07:43:34 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes the value of a poier and assign it to an integer
+ * @n: integer 
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function that swaps the value of two integers
+ * @a: - first integer
+ * @b: second integer
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int y;
+	y = *a;
+	*a = *b;
+	*b = y;
+
+}
