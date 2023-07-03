ad08fe2ec70acea8f3ed8fceb108cb3e5ed69f32
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-07-03 13:51:36 -0700

    cool

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - a function that locates a character in a string
@@ -17,7 +16,7 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 
 
 
