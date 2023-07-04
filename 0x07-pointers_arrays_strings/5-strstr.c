fd8f9287a76bcf0833e3a57e43ea3c403c2d9a04
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-07-04 15:09:39 -0700

    cool

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,8 +14,8 @@ char *_strstr(char *haystack, char *needle)
 
 		while (*haystack != '\0')
 		{
-			 h = haystack;
-			 n = needle;
+			h = haystack;
+			n = needle;
 			while (*n != '\0' && *haystack == *n)
 			{
 				haystack++;
