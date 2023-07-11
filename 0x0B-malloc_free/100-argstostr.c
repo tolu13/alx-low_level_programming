a3ad8254cf1a37a8e0564f57f6d5ccdb0bd1d3d5
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-07-11 15:22:58 -0700

    cool

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,4 +2,45 @@
 #include <stdio.h>
 #include <Stdlib.h>
 /**
- * 
+ * argstostr - function that concatenates all program
+ * @ac: arguement count
+ * @av: arguement vector
+ * Return: pointer to array of char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *f;
+	int r, s, t, u;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (r = 0; r < ac; r++)
+	{
+		if (av[s] == NULL)
+			return (NULL);
+
+		for (t = 0; av[s][t] != '\0'; t++)
+			r++;
+		r++;
+
+	}
+	f = malloc((r + 1) * sizeof(char));
+
+	if (f == NULL)
+	{
+		free(f);
+		return (NULL);
+	}
+	for (s = t = u = 0; u < r s++, u++)
+	{
+		f[u] = '\n';
+		s++;
+		u++;
+		t = 0;
+	}
+	if (u < r - 1)
+		f[u] = av[s][t];
+
+	return (f);
+}
