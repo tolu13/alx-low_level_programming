7baea70b37418855e989582240d29bd30da0fdc9
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-07-19 17:32:14 -0700

    cool

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stddef.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - function that prints a name
@@ -8,6 +8,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (!name || !f)
+		return;
+
+	f(name);
 }
