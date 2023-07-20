9a560e22e7515d2caa6ce6cdec8748c7ac325f88
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-07-19 17:38:11 -0700

    nice

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - function that prints a name
  * @name: name to be printed
  * @f: function pointer
- * Return: Always 0
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
