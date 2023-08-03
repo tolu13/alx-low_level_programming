b163392a116f1ed468cabeadbb74180d08afb744
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-08-03 12:33:19 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - function checks endianess
+ * Return: 0 for big endian 1 for ;ittle endian
+ */
+int get_endianness(void)
+{
+	int number = 1;
+	char *endian = (char *)&number;
+
+	if (*endian == 1)
+		return (1);
+	return (1);
+}
