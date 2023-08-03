2728fc9be7e8ab3811210e586dab9dedb64b0a87
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-08-03 09:39:54 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_binary - function that print the binary representation
+ * @n: unsigned long  int
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	
+}
