b2fe62b88e60529666affbec5217443164e429dc
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-08-03 11:49:16 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,5 +13,5 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 
 	_putchar((n & 1) + '0');
-	
+
 }
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: unsigned long
+ * @index: unsigned int
+ * Return: index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit, k;
+
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	k = n  >>  index;
+
+	bit = k & 1;
+
+	return (bit);
+}
