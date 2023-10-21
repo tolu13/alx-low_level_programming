65a74a6cea9d83d693f04c32776d7d194c699ad3
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-10-21 22:13:02 +0000

    shared libraries

---
+++ 0x18-dynamic_libraries/_strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - This function copies a string pointed to src and dest
+ * @dest: pointer parameter
+ * @src: pointer paramter
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
